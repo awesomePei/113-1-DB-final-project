dacb23b8ad72f60934b44908c29b9c8219eac365
zzxjww <b11705015@ntu.edu.tw>
2024-12-08 22:55:30 +0800

    same seatID okay

--- ticketvibe/db.py
+++ ticketvibe/db.py
@@ -3,9 +3,9 @@ from psycopg2.extras import RealDictCursor
 
 def get_connection():
     return psycopg2.connect(
-        dbname="ticketvibe",
-        user="",
-        password="",
+        dbname="DB Final",
+        user="postgres",
+        password="0000",
         host="localhost",
         port="5432"
     )
