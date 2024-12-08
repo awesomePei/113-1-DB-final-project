2f42567a7649f72a705321d83b13ea900f5bc510
awesomePei <momotsai910@gmail.com>
2024-12-08 17:10:32 +0800

    Just for pulling

--- ticketvibe/app.py
+++ ticketvibe/app.py
@@ -351,4 +351,3 @@ def logout():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- ticketvibe/db.py
+++ ticketvibe/db.py
@@ -3,7 +3,7 @@ from psycopg2.extras import RealDictCursor
 
 def get_connection():
     return psycopg2.connect(
-        dbname="DB Final",
+        dbname="ticketvibe",
         user="postgres",
         password="0000",
         host="localhost",
